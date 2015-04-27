09b2e85500dcc787aaa99e3044a76d2a46c3cda3
jdf258 <jdf258@cornell.edu>
2015-04-27 10:30:02 -0400

    Take 2

---
+++ application/Debug/simple_send_app.pp
@@ -0,0 +1,16 @@
+# FIXED
+simple_send_app.obj: ../simple_send_app.c
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/bsp/bsp.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/bsp/boards/EZ430RF/bsp_board_defs.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/bsp/mcus/bsp_msp430_defs.h
+simple_send_app.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/msp430/include/msp430.h
+simple_send_app.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/msp430/include/msp430x22x4.h
+simple_send_app.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/msp430/include/In430.h
+simple_send_app.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/msp430/include/intrinsics.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/bsp/boards/EZ430RF/bsp_config.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/bsp/bsp_macros.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/mrfi/mrfi.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/mrfi/mrfi_defs.h
+simple_send_app.obj: C:/Users/Jay/Documents/workspace/ECE3140_Final_Project/application/../drivers/mrfi/radios/family1/mrfi_spi.h
+simple_send_app.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/msp430/include/string.h
+simple_send_app.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/tools/compiler/msp430/include/linkage.h
